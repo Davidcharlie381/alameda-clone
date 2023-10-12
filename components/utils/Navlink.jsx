f299ebc1c83c0f2ed8b71736e18b87851727122b
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-10-12 09:12:29 +0100

    Update Navlink.jsx

--- components/utils/Navlink.jsx
+++ components/utils/Navlink.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 
-const Navlink = ({name, path, className}) => {
+const Navlink = ({name, path, className, setNavOpen}) => {
 
     const pathname = usePathname();
 
     const isActive = pathname === path;
 
     return (
-        <Link href={path} className={`${isActive && "underline underline-offset-[9px]"} ${className}`}>{name}</Link>
+        <Link href={path} onClick={() => setNavOpen(false)} className={`${isActive && "underline underline-offset-[9px]"} ${className}`}>{name}</Link>
     )
 }
 
