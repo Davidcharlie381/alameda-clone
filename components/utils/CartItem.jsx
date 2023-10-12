6b4466f7b3a83678cf37874e61140f3d480769e9
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-10-12 15:47:28 +0100

    feat: async dispatch

--- components/utils/CartItem.jsx
+++ components/utils/CartItem.jsx
@@ -13,6 +13,12 @@ const CartItem = ({item}) => {
 
   const dispatch = useDispatch();
 
+  const dispatchAsync = (func) => {
+    setTimeout(() => {
+      dispatch(func);
+    }, 1000)
+  }
+
   const { name, images, slug, price, newSize } = item;
 
   const itemExist = useSelector((state) => state.cart.find((item) => item.slug === slug));
@@ -23,7 +29,7 @@ const CartItem = ({item}) => {
 
   return (
     <div className="mb-5 relative">
-      <button className="absolute top-0 right-0 rounded-full h-8 w-8 hover:bg-gray-200 grid place-content-center" onClick={() => dispatch(removeFromCart(item))}>
+      <button className="absolute top-0 right-0 rounded-full h-8 w-8 hover:bg-gray-200 grid place-content-center" onClick={() => dispatchAsync(removeFromCart(item))}>
         <BsXLg />
       </button>
       <div className="flex gap-5 mb-5">
@@ -39,12 +45,12 @@ const CartItem = ({item}) => {
           </div>
           <div className="flex justify-between md:justify-end gap-16 md:w-2/5">
             <div className="flex gap-3 ">
-              <button className="rounded-full h-8 w-8 hover:bg-gray-200 grid place-content-center" onClick={() => dispatch(decrementQuantity(item))}>
+              <button className="rounded-full h-8 w-8 hover:bg-gray-200 grid place-content-center" onClick={() => dispatchAsync(decrementQuantity(item))}>
                 <AiOutlineMinus />
               </button>
 
               <span className="mt-1 block">{qty}</span>
-              <button className="rounded-full h-8 w-8 hover:bg-gray-200 grid place-content-center" onClick={() => dispatch(incrementQuantity(item))}>
+              <button className="rounded-full h-8 w-8 hover:bg-gray-200 grid place-content-center" onClick={() => dispatchAsync(incrementQuantity(item))}>
                 <BsPlusLg />
               </button>
             </div>
