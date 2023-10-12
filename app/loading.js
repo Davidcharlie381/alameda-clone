3eba230deba501e88bf227fcc955ac72f7e775ce
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-10-12 14:39:05 +0100

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <main className="container grid place-content-center">
+    <main className="container grid place-content-center mt-14 md:mt-20">
     <div className="h-10 w-10 rounded-full border-2 bg-transparent border-t-black animate-spin">
     </div>
     </main>
