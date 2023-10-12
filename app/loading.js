1cc3d860d02ab194a349b11315038cbd5a7652d9
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-10-12 14:25:11 +0100

    Create loading.js

---
+++ app/loading.js
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <main className="container grid place-content-center">
+    <div className="h-20 w-20 rounded-full border-2 bg-transparent border-t-black animate-spin">
+    </div>
+    </main>
+  )
+}
+
+export default Loading;
