2900c012690de2fa325af6e3e5c2a2680e41c3a6
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-10-12 14:54:02 +0100

    Update page.js

--- app/lookbook/page.js
+++ app/lookbook/page.js
@@ -1,5 +1,12 @@
 const Lookbook = () => {
-  return <main className="container">Lookbook coming soon</main>;
-};
+  return (
+    <main className="container grid place-content-center mt-14 md:mt-20">
+<div className="text-center">
+    <h2 className="text-2xl font-semibold font-roboto mb-4">Coming sooon!</h2>
+        </div>
+
+    </main>
+    )
+}
 
 export default Lookbook;
